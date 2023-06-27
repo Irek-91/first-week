813a9ed290df9e407389a401f2a678053ee9dd05
Irek Shamilov <shamilov.irek@bk.ru>
2023-06-27 15:31:22 +0300

    5

--- dist/index.js
+++ dist/index.js
@@ -121,6 +121,14 @@ app.put('/videos/:id', (req, res) => {
             "field": "publicationDate"
         });
     }
+    for (let i = 0; i < availableResolutions.length; i++) {
+        if (permissionVariants.includes(availableResolutions[i]) === false) {
+            apiErrorResult.push({
+                "message": 'availableResolutions',
+                "field": "availableResolutions"
+            });
+        }
+    }
     if (apiErrorResult.length !== 0) {
         res.sendStatus(400).send({ errorsMessages: apiErrorResult });
     }
